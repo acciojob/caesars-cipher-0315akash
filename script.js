791a04a27d570ec940a496500227032b72e91185
0315akash <124701907+0315akash@users.noreply.github.com>
2023-05-17 21:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Your Script here.
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+function rot13(str) {
+  // Create an array to store the decoded characters
+  let decodedArr = [];
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  // Loop through each character in the input string
+  for (let i = 0; i < str.length; i++) {
+    // Get the ASCII code of the current character
+    let charCode = str.charCodeAt(i);
 
-  return ;//return decodedArr
+    // Decode uppercase letters (A-Z)
+    if (charCode >= 65 && charCode <= 90) {
+      // Apply the ROT13 shift by adding 13 to the character code
+      let decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+      
+      // Convert the decoded character code back to a letter and add it to the decoded array
+      decodedArr.push(String.fromCharCode(decodedCharCode));
+    } else {
+      // For non-alphabetic characters, pass them on without decoding
+      decodedArr.push(str[i]);
+    }
+  }
+
+  // Join the decoded array into a string and return it
+  return decodedArr.join('');
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
+// Example usage
+let encodedString = "EBG13 rknzcyr.";
+let decodedString = rot13(encodedString);
+console.log(decodedString);  // Output: ROT13 example.
